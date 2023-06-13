07c62926b0a0510919be99971daeddc063d76ee1
GabrieleGrossi <grossigabriele98@gmail.com>
2023-06-13 14:29:54 +0200

    create mounted in js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,21 @@
 // DESCRIZIONE
 // Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            emailList : []
+        }
+    },
+    mounted() {
+        for (let i = 0 ; i <= 9 ; i++) {
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response =>
+                    this.emailList.push(response.data.response))    
+        }
+    }
+}).mount('#app');
